39312606c9e0d2152572f7544c11db0cf545f1c5
seba133 <sebote45@hotmail.com>
2015-05-11 23:41:45 -0300

    Update SysVentasSQL.sql

--- MySQL/SysVentasSQL.sql
+++ MySQL/SysVentasSQL.sql
@@ -3,3 +3,8 @@ use SysVentas;
 create table if not exists administrador(
 
 ));
+create table if not exists cliente(rut varchar(20),
+                    nombre varchar(20),
+                    apellido varchar(20),
+                    ciudad varchar(20),
+                    primary key(rut));
